758b3b9acbd161d81fe3b464913fa84ce3a8ffd7
Pawel Duminski <pduminski@gmail.com>
2023-04-01 19:08:08 +0200

    4-module-homework comments update

--- cypress/e2e/4-module-homework/module3Homework.cy.js
+++ cypress/e2e/4-module-homework/module3Homework.cy.js
@@ -6,27 +6,29 @@ describe("Module 4 Homework - Locators", () => {
   });
 
   it("find 5 elements with: ID", () => {
-    cy.get("#content"); // whole content to work on that element
-    cy.get("#nv-primary-navigation-main"); // top nav bar
-    cy.get("#menu-item-923"); // top nav bar element: Newsletter
-    cy.get("#menu-item-1871"); // top nav bar element: Kursy
-    cy.get("#menu-item-1023"); // top nav bar element: Webinary
+    cy.get("#content"); 
+    cy.get("#nv-primary-navigation-main");
+    cy.get("#menu-item-923");
+    // top nav bar element: Kursy
+    cy.get("#menu-item-1871"); 
+    // top nav bar element: Webinary
+    cy.get("#menu-item-1023"); 
   });
 
   it("find 5 elements with: class", () => {
-    cy.get(".elementor-button-link").first(); // sprawdz nasze kursy button 1 on the top of the page
-    cy.get(".elementor-button-link").last(); // sprawdz nasze kursy button 2 on the bottom of the page
-    cy.get("nav.header--row"); // top header
-    cy.get("div.elementor-container.elementor-column-gap-no"); // div with Kompletny Proces / Wsparcie Mentora / Krótkie, treściwe lekcje / Gwarancja jakości
-    cy.get("div.elementor-widget-image-gallery"); // div with te list of images of companies
+    cy.get(".elementor-button-link").first();
+    cy.get(".elementor-button-link").last();
+    cy.get("nav.header--row");
+    cy.get("div.elementor-container.elementor-column-gap-no");
+    cy.get("div.elementor-widget-image-gallery");
   });
 
   it("find 5 elements using: element with attribute", () => {
     cy.get("a[href='https://fabrykatestow.pl/dokumenty']");
-    cy.get("a[href='https://fabrykatestow.pl/git']"); // GIT DLA TESTERA
-    cy.get("a[href='#kursy']"); // first button for Sprawdź nasze kursy
-    cy.get("div[class='elementor-widget-container']").first(); // Nie trać czasu! Automatyzuj swoje testy!
-    cy.get("img[data-variant = 'logo']"); // logo in the left top corner
+    cy.get("a[href='https://fabrykatestow.pl/git']");
+    cy.get("a[href='#kursy']");
+    cy.get("div[class='elementor-widget-container']").first();
+    cy.get("img[data-variant = 'logo']");
   });
 
   it("find 5 elements using: text", () => {
