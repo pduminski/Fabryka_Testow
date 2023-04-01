24ef08acb4db8fd61aac1eb9aef2f48d81dcc7a5
Pawel Duminski <pduminski@gmail.com>
2023-04-01 02:24:01 +0200

    test case written from scratch

--- cypress/e2e/4-module-5-Learning/TS1.cy.js
+++ cypress/e2e/4-module-5-Learning/TS1.cy.js
@@ -1,7 +1,9 @@
-describe("my first scenario", () => {
-  it("Visit main page and click top menu button Newsletter", () => {
-    cy.visit("https://fabrykatestow.pl");
-    cy.get("#nv-primary-navigation-main").contains("Newsletter").click();
-    cy.url().should("eq", "https://fabrykatestow.pl/ciekawostki/");
-  });
+/// <reference types="cypress" />
+
+describe('My first test case from scratch', () => {
+    it('Visit main page and click top menu button Newsletter', () => {
+        cy.visit("https://fabrykatestow.pl/");
+        cy.get("#nv-primary-navigation-main").contains("Newsletter").click();
+        cy.url().should("eq", "https://fabrykatestow.pl/ciekawostki/");       
+    });
 });
